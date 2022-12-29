e7c3c6579211bd7d9398aaa783e92fba58967edd
Madis Kõosaar <madis.koosaar@solidify.dev>
2022-12-29 15:40:50 +0200

    Update write-comment.js

--- .github/scripts/write-comment.js
+++ .github/scripts/write-comment.js
@@ -1,1 +1,16 @@
 
+const { Octokit } = require('@octokit/core');
+
+module.exports = ({context, core, options}) => {
+
+    const octokit = new Octokit({
+        auth: `${options.Token}`
+      })
+      
+      await octokit.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: issue.number,
+        body: '/run-production-migration'
+      })
+}
